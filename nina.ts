c861fa2fa7c6e44af5a2eb4b1d4957a926df7f31
David Kouril <david.kouril@gmail.com>
2024-07-21 11:58:25 +0200

    old commits

--- nina.ts
+++ nina.ts
@@ -1,6 +1,17 @@
-import { parse, stringify } from "jsr:@std/yaml";
+import { parse } from "jsr:@std/yaml";
+import { dayOfYear, parse as dateTimeParse } from "@std/datetime";
 
+const d = dayOfYear(new Date("2019-03-11T03:24:00")); // 70
+console.log(d);
 const ninaYAML = await Deno.readTextFile("data/nina.yaml");
 const ninaData = parse(ninaYAML);
 
-console.log(ninaData);
+const ninaDataJson = JSON.stringify(ninaData);
+await Deno.writeTextFile("data/nina.json", ninaDataJson);
+
+// for (const day of ninaData.days) {
+// 	console.log(day.bedtime);
+// 	// const time = dateTimeParse(day.bedtime, "hh:mm");
+// 	// console.log(time);
+// }
+
