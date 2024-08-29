83522bc5e7c6de05b1236670bda7c3acaa34bf24
David Kouril <david.kouril@gmail.com>
2024-08-29 09:54:28 -0400

    chore: deno task for converting, separate folder for output data

--- nina.ts
+++ nina.ts
@@ -1,8 +1,6 @@
 import { parse } from "jsr:@std/yaml";
 import { dayOfYear, parse as dateTimeParse } from "@std/datetime";
 
-// const d = dayOfYear(new Date("2019-03-11T03:24:00")); // 70
-// console.log(d);
 const ninaYAML = await Deno.readTextFile("data/nina.yaml");
 const ninaData = parse(ninaYAML);
 
@@ -12,10 +10,4 @@ const m = tdy.getMonth() + 1;
 const y = tdy.getFullYear();
 const tdyStamp = `${y}-${m}-${d}`;
 const ninaDataJson = JSON.stringify(ninaData);
-await Deno.writeTextFile(`data/nina-${tdyStamp}.json`, ninaDataJson);
-
-// for (const day of ninaData.days) {
-// 	console.log(day.bedtime);
-// 	// const time = dateTimeParse(day.bedtime, "hh:mm");
-// 	// console.log(time);
-// }
+await Deno.writeTextFile(`data/processed/nina-${tdyStamp}.json`, ninaDataJson);
